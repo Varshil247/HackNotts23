8a9ae8afc14a7d9e519fcf9ea550b9bb7b90faa2
Arth253 <arthgandhi2531@gmail.com>
2023-02-11 21:30:54 +0000

    added graphs

---
+++ ideas/GUI/graph.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+
+Builder.load_file('design.kv')
+#Builder.load_file('variableLablel.kv')
+
+class MyLayout(Widget):
+    pass
+
+class MyApp(App):
+    def build(self):
+        Window.clearcolor = (1,1,1,1)
+        return MyLayout()
+    
+    
+if __name__ == '__main__':
+    MyApp().run()
